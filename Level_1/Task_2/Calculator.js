8e55f89c1b9382fd62aaea8579cc38ef0111fc17
Rajan Kumar <raorajan9576@gmail.com>
2024-02-05 16:23:01 +0530

    changed the layout of the computer

--- Level_1/Task_2/Calculator.js
+++ Level_1/Task_2/Calculator.js
@@ -15,3 +15,20 @@ function calculate() {
   currentDisplay = eval(currentDisplay);
   document.querySelector('#display').value = currentDisplay;
 }
+
+function calculateSquareRoot() {
+    currentDisplay = Math.sqrt(eval(currentDisplay));
+    document.querySelector('#display').value = currentDisplay;
+  }
+  
+  function calculatePercentage() {
+    currentDisplay = eval(currentDisplay) / 100;
+    document.querySelector('#display').value = currentDisplay;
+  }
+  
+  function clearLastEntry() {
+    // Remove the last character from the current display
+    currentDisplay = currentDisplay.slice(0, -1);
+    document.querySelector('#display').value = currentDisplay;
+  }
+  
