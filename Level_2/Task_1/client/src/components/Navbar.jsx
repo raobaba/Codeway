26debcc1870c066b47afa46c76b38f0d42703a53
Rajan Kumar <raorajan9576@gmail.com>
2024-02-06 11:08:46 +0530

    created respsonsive navbar

--- Level_2/Task_1/client/src/components/Navbar.jsx
+++ Level_2/Task_1/client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,13 +12,29 @@ const Navbar = () => {
   const closeNavbar = () => {
     setIsOpen(false);
   };
+  useEffect(() => {
+    const handleResize = () => {
+      // Close the navbar if the screen size is greater than 600px
+      if (window.innerWidth > 600) {
+        setIsOpen(false);
+      }
+    };
+
+    // Attach the event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); 
 
   return (
     <div className={`navbar ${isOpen ? 'open' : ''}`}>
-      <div className="navbar-left">
-        Job
+      <div className="navbar-left" onClick={closeNavbar}>
+        Codeway
       </div>
-      <button className="toggle-btn" onClick={toggleNavbar}>
+      <button className={`toggle-btn ${isOpen ? 'open' : ''}`} onClick={toggleNavbar}>
         <span></span>
         <span></span>
         <span></span>
